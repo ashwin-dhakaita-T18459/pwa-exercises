423d552db95889fe66412da8f7fee93f56a8b9a6
Ashwin Dhakaita <ashwin.dhakaita@freshworks.com>
2021-04-16 17:20:39 +0530

    use mockend for mock requests

--- exercise2/js/script.js
+++ exercise2/js/script.js
@@ -2,6 +2,23 @@ async function getapi(url) {
     
     // Storing response
     const response = await fetch(url);
+
+
+    //Mock request for checking with the response from mockend
+    const response2 = await fetch("https://mockend.com/ashwin-dhakaita-T18459/pwa-exercises/posts").then(response => response.json()).then(json => console.log(json))
+
+    // const response2 = await fetch("https://aafreshteam2779.freshhr.com/users?filter_id=&page=1&per_page=14&sort=first_name&sort_type=asc&query_hash%5B0%5D%5Bcondition%5D=record_status&query_hash%5B0%5D%5Boperator%5D=is_in&query_hash%5B0%5D%5Bvalue%5D=2", {
+    //     method: "GET",
+    //     headers: {
+    //         "User-Agent": "freshteam_mobile_ios/2.0",
+    //         "Authorization": "JWTAuth token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4R0NNIn0..pO1K7dL9U0KnVSG9.hy7KW0nFx_1RuFY4XCXs3woqUiC46k8_Xse4IobYDLNi_KuN7etLBWQA3DAbWTRseHn9P2L7aFSB4A8XQPM4qjp_UcJpTlrxd4YwXdW989cmYC5r3sEQol04Gsti0QL3xRhTqoIK8rSdvbsTV4b7wB--ryFV86ZYKaM6lN4XqH9PodQHHujMrKlEiehFRx3RwKcyqRhgZIEIuJXwuE_8VmOdpV1GQMIlhTBSeYXi2CqX.yX8zJPrQP-py7vwVC9nM7g",
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json",
+    //         },
+    //     //referrerPolicy: 'no-referrer',
+    //     credentials: "omit",
+    //     mode: "no-cors"
+    // });
     
     // Storing data in form of JSON
     var data = await response.json();
